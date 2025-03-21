8b2dbd58187ae5f883f919a08b161a227b130d94
sxbecki-pjait <s33693@pjwstk.edu.pl>
2025-03-21 11:11:08 +0100

    Product commit 2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,5 @@
 public class Main {
     public static void main(String[] args) {
         Product p1 = new Product("Nike Air Force 1 white", 399.99,"clothes", 46);
-        System.out.println(p1.name + " " + p1.id);
     }
 }
--- src/Product.java
+++ src/Product.java
@@ -2,15 +2,47 @@ import javax.xml.crypto.dsig.keyinfo.KeyValue;
 import java.time.LocalDateTime;
 
 public class Product {
-    public String name;
-    public String id;
-    public String category;
-    public double price;
-    public int stockQuantity;
+    private String name;
+    private String id;
+    private String category;
+    private double price;
+    private int stockQuantity;
     public Product(String newName, double newPrice, String newCategory, int newStockQuantity) {
         name = newName;
         id = LocalDateTime.now().toString();
 
-
     }
+
+    public String getName() {
+        return name;
+    }
+    public String getId() {
+        return id;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+    public void setName(String newName) {
+        name = newName;
+    }
+    public void setId(String newId) {
+        id = newId;
+    }
+    public void setCategory(String newCategory) {
+        category = newCategory;
+    }
+    public void setPrice(double newPrice) {
+        price = newPrice;
+    }
+    public void setStockQuantity(int newStockQuantity) {
+        stockQuantity = newStockQuantity;
+    }
+
+
 }
